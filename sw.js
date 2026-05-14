8598fa567ede22057c0817adc07427a7efb1bf76
John H <info@johnhayn.es>
2026-05-14 14:52:40 +0100

    more photos

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline-first service worker. Cache the shell + CDN deps so the app works
 // in patchy signal on the island.
-const CACHE = 'skye-wildlife-v21';
+const CACHE = 'skye-wildlife-v22';
 const SHELL = [
   './',
   './index.html',
@@ -38,7 +38,7 @@ self.addEventListener('fetch', e => {
       if (hit) return hit;
       return fetch(e.request).then(res => {
         // Opportunistically cache same-origin and font-CDN GETs.
-        if (res.ok && (e.request.url.startsWith(self.location.origin) || /fonts\.gstatic|jsdelivr|upload\.wikimedia\.org|en\.wikipedia\.org\/api/.test(e.request.url))) {
+        if (res.ok && (e.request.url.startsWith(self.location.origin) || /fonts\.gstatic|jsdelivr|upload\.wikimedia\.org|en\.wikipedia\.org\/api|commons\.wikimedia\.org/.test(e.request.url))) {
           const clone = res.clone();
           caches.open(CACHE).then(c => c.put(e.request, clone));
         }
