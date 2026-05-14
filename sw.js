61ca5d0e7702fcf9429dbb6ececc5b1ffebfb8ee
John H <info@johnhayn.es>
2026-05-14 14:02:30 +0100

    updates to layout

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline-first service worker. Cache the shell + CDN deps so the app works
 // in patchy signal on the island.
-const CACHE = 'skye-wildlife-v13';
+const CACHE = 'skye-wildlife-v15';
 const SHELL = [
   './',
   './index.html',
