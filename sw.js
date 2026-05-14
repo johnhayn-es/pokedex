88449cc1bbbf363f216d61431090ef8ac7b7bf54
John H <info@johnhayn.es>
2026-05-14 14:45:08 +0100

    header

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline-first service worker. Cache the shell + CDN deps so the app works
 // in patchy signal on the island.
-const CACHE = 'skye-wildlife-v20';
+const CACHE = 'skye-wildlife-v21';
 const SHELL = [
   './',
   './index.html',
